3926fa84ca3326b5fee72d0eaeb6d6397e55a32d
realmind87 <realmind87@naver.com>
2019-03-22 10:01:14 +0900

    commit

--- transition/flipapp/src/App.vue
+++ transition/flipapp/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <button v-on:click="show = !show">toggle</button>
-    <transition name="fade">
-      <p v-if="show">hello</p>
+    <button v-on:click="show = !show">Toggle show</button>
+    <transition name="bounce">
+      <p v-if="show">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis enim libero, at lacinia diam fermentum id. Pellentesque habitant morbi tristique senectus et netus.</p>
     </transition>
   </div>
 </template>
@@ -15,10 +15,21 @@ export default {
 </script>
 
 <style>
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
+.bounce-enter-active {
+  animation: bounce-in .5s;
 }
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
+.bounce-leave-active {
+  animation: bounce-in .5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
