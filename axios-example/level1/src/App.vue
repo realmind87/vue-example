35e6577c99e5e67afc2262478c908dc3695eeb0f
realmind87 <realmind87@naver.com>
2019-04-03 22:35:24 +0900

    axios test

---
+++ axios-example/level1/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <AppAxiosTest/>
+  </div>
+</template>
+
+<script>
+import AppAxiosTest from './components/AppAxiosTest'
+export default {
+  name: 'App',
+  components: { AppAxiosTest }
+}
+</script>
+
+<style>
+</style>
