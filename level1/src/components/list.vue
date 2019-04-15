3cca4439a48db6d1715d2ebc36ade5f529806fb8
realmind87 <realmind87@naver.com>
2019-04-16 01:10:52 +0900

    asdfasdf

---
+++ level1/src/components/list.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <table border='1'>
+      <thead>
+          <tr>
+            <th>이름</th><th>전화번호</th><th>주소</th><th>사진</th>
+          </tr>
+      </thead>
+      <tbody>
+          <tr v-for='contacts in contactlist.contacts'>
+            <td>{{ contacts.name }}</td>
+            <td>{{ contacts.tel }}</td>
+            <td>{{ contacts.address }}</td>
+            <td><img arc="" v-bind:src="contacts.photo" /></td>
+          </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'list',
+  props : ['contactlist'],
+  data () {
+    return {}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
