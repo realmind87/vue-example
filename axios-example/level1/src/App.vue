952a207c0fb9abc75ed5a6d9583530c09ef2ee68
realmind87 <realmind87@naver.com>
2019-05-08 23:40:37 +0900

    st

--- axios-example/level1/src/App.vue
+++ axios-example/level1/src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <div id="app">
+    <list />
   </div>
 </template>
 
 <script>
+import List from '@/components/list'
+
+export default {
+    name : 'app',
+    components : { List }
+}
 </script>
 
 <style>
+  * {margin:0;padding:0;}
+  li {list-style: none;}
+  table {width:100%;border-collapse:collapse; border-spacing:0;}
 </style>
