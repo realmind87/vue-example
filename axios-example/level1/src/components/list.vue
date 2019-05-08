89f0397be9a654213c1e715de1a0c4e81eefff87
realmind87 <realmind87@naver.com>
2019-05-08 23:00:04 +0900

    dw

---
+++ axios-example/level1/src/components/list.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'list',
+  props : ['contactlist'],
+  data () {
+    return {}
+  },
+  mounted() {
+    
+  },
+  methods: {
+    
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
